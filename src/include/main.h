606740b131fef6f5687c8bc008300e2f028a37bf
ARM9 <toast.dslua@gmail.com>
2013-04-16 04:45:51 +0200

    tryna fix it up

---
+++ src/include/main.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <snes.h>
+
+u16 pad0, pad1;
+
+typedef struct{
+	u8 some;
+	u8 test;
+	u16 data;
+}Player_s;
--- src/main.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <snes.h>
-
-u16 pad0, pad1;
