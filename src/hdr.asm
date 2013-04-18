b1810d94213045c311e21effdd631f90526518cd
ARM9 <toast.dslua@gmail.com>
2013-04-18 19:17:05 +0300

    Updated hdr.asm

--- src/hdr.asm
+++ src/hdr.asm
@@ -21,16 +21,15 @@
   SLOWROM ; SLOWROM or FASTROM
   LOROM ; LOROM or HIROM, LOROM banks must be $8000 (32KB) and HIROM banks must be $10000 (64KB)
 
-  CARTRIDGETYPE $00             ; $00 = ROM only, ; $00 ROM only, $01 ROM and RAM no sram, $02 ROM and sram. higher than $02 indicates special hardware addon in cartridge, must set rom layout properly, see line 21 and 22
+  CARTRIDGETYPE $00   ; $00 ROM only, $01 ROM and RAM no sram, $02 ROM and sram. higher than $02 indicates special hardware addon in cartridge, must set rom layout properly, see line 21 and 22
   ; $00 to $0C RAM and ROM size. Values for RAM are $00 (no RAM) to $05 (32KB)
-  ; $06 and $07 are apparently unused but are indicated as 64KB and 128KB respectively
+  ; RAM values $06 and $07 are apparently unused but are indicated as 64KB and 128KB respectively
   ; $08 to $0C ROM size, $08 is 256KB (minimum allowed ROM size by hardware) and $0C 4MB
-  ROMSIZE $08                   ; $08 = 2 Mbits, $08 to $0C, 256KB to 4MB, MUST CHANGE LABELS .ROMBANKSIZE AND .ROMBANKS
-  SRAMSIZE $00                  ; No SRAM         see WLA doc for more.. ; $01 to $05    bytes $06 and $07 are unused
-  ;  country code $00 => Japanese, $01 => NTSC, $02 to $0c => PAL, $0d => NTSC
-  COUNTRY $01                   ; $01 = U.S.  $00 = Japan, that's all I know
-  LICENSEECODE $00              ; use $00
-  VERSION $00                   ; $00 = 1.00, $01 = 1.01, etc.
+  ROMSIZE $08         ; $08 = 2 Mbits, $08 to $0C, 256KB to 4MB, MUST CHANGE LABELS .ROMBANKSIZE AND .ROMBANKS
+  SRAMSIZE $00        ; No SRAM         see WLA doc for more.. ; $01 to $05    bytes $06 and $07 are unused
+  COUNTRY $01         ; country code $00 => Japanese, $01 => NTSC, $02 to $0c => PAL, $0d => NTSC
+  LICENSEECODE $00    ; use $00
+  VERSION $00         ; $00 = 1.00, $01 = 1.01, etc.
 .ENDSNES
 
 ; .COMPUTESNESCHECKSUM to make checksum, shall investigate
